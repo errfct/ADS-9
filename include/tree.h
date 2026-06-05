3149ab32ce0419c5ee9547baadf3f81806411516
errfct <veronika.sokolova.07@mail.ru>
2026-06-05 23:11:00 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,29 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
-#endif  // INCLUDE_TREE_H_
+#include <vector>
+
+struct TreeNode {
+    char value;
+    std::vector<TreeNode*> children;
+
+    TreeNode(char val) : value(val) {}
+    ~TreeNode();
+};
+
+class PMTree {
+private:
+    TreeNode* root;
+    void buildTree(TreeNode* node, const std::vector<char>& elements);
+
+public:
+    PMTree(const std::vector<char>& elements);
+    ~PMTree();
+    TreeNode* getRoot() const { return root; }
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
+#endif // INCLUDE_TREE_H_
